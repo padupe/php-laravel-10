05e2be6877308f69438591505164191ab4396ce3
Paulo Eduardo Peixoto <peixoto.pauloeduardo@gmail.com>
2023-10-31 08:45:15 -0300

    feat(database/migrations): create migration from "Supports".
    Signed-off-by: Paulo Eduardo Peixoto <peixoto.pauloeduardo@gmail.com>

---
+++ database/migrations/2023_10_31_111730_create_supports_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('supports', function (Blueprint $table) {
+            $table->id();
+            // String == varChar
+            $table->string('subject');
+            $table->enum('status', ['active', 'pendent', 'closed']);
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('supports');
+    }
+};
