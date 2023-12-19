65fe922e126913462e9f7c08342b85a814203629
Paulo Eduardo Peixoto <peixoto.pauloeduardo@gmail.com>
2023-12-19 08:55:20 -0300

    feat(routes/web.php): create routes "supports/create" [GET] and "supports" [POST].
    Signed-off-by: Paulo Eduardo Peixoto <peixoto.pauloeduardo@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use App\Http\Controllers\Site\SiteController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
+Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
 Route::get('/supports', [SupportController::class, 'index'])->name('supports/index');
 
 Route::get('/contato', [SiteController::class, 'contact']);
